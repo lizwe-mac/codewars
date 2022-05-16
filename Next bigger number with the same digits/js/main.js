37f83f0a203f386d9e801f1f35e199dc4d5a4ada
MacDonald Ginyani <68953760+theorem23@users.noreply.github.com>
2022-05-16 16:43:04 +0200

    some changes

--- Next bigger number with the same digits/js/main.js
+++ Next bigger number with the same digits/js/main.js
@@ -1,18 +1,22 @@
 function nextBigger(n){
     const num_string_array = String(n).split('')
+    // console.log(String(n).split(''))
     let possible_nums = []
     for (let i=0; i<num_string_array.length;i++){
         let first_num = [num_string_array[i]]
         let other_nums = num_string_array.slice(0,i).concat(num_string_array.slice(i+1))
-        console.log(first_num)
-        console.log(other_nums)
+        
         possible_nums.push(Number(first_num.concat(other_nums).join(''))) 
         let new_array = first_num.concat(other_nums)
-        for(let j=1;j<new_array.length;j++){
-            console.log('other_nums')
-
-            let first_num = [num_string_array[j]]
-            let other_nums = num_string_array.slice(0,j).concat(num_string_array.slice(j+1))
+        for(let j=1;j<new_array.length-1;j++){
+            // console.log('other_nums')
+            
+            let first_num = [new_array[j]]
+            let other_nums = new_array.slice(0,j).concat(num_string_array.slice(j+1))
+            console.log(new_array)
+            console.log(first_num)
+            console.log(other_nums)
+            console.log(num_string_array.slice(j+1))
             possible_nums.push(Number(other_nums.concat(first_num).join(''))) 
         }
     }
