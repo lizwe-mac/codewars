5dfc9315dd3dd845b10a1fc4e0eb22d568987918
MacDonald Ginyani <68953760+theorem23@users.noreply.github.com>
2022-05-19 22:58:35 +0200

    od

--- ROT13/js/main.js
+++ ROT13/js/main.js
@@ -2,7 +2,7 @@
 //Parameters - message (words) in the form of the latin/Engish alphabet
 //Return - a ROT13 ciphered message
 //Example - parameter: 'abc' , return: 'nop'
-//Pseudocode - 1. create alphabet object, 2. parse each letter in the message and map it to a different location in the alphabet, return ciphered message 
+//Pseudocode - 1. create alphabet object, 2. parse each letter in the message and map it to a different location in the alphabet, return ciphered message rererer
 
 const alphabet = {a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13,n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26}
 function rot13(message){
