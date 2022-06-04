d48029a06c2d779d3b6d00c408cd92a000088fe7
MacDonald Ginyani <68953760+theorem23@users.noreply.github.com>
2022-06-04 14:50:48 +0200

    Completed kata

--- Perfect squares perfect fun/js/main.js
+++ Perfect squares perfect fun/js/main.js
@@ -1,1 +1,18 @@
+function squareIt(int) {
+  return Math.sqrt(String(int).length) % 1 !== 0
+    ? "Not a perfect square!"
+    : printSquares(int);
+}
 
+function printSquares(int) {
+  let arr = [];
+  let i = 0;
+  const square = String(int).length;
+  const squareNum = Math.sqrt(String(int).length);
+  do {
+    console.log(i);
+    arr.push(String(int).slice(i, i + squareNum));
+    i = i + squareNum;
+  } while (i < square);
+  return arr.join("\n");
+}
